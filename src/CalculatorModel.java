58fc6db4f4cb502018bb919b56d7f76fc2954e40
Yanis <yanisblotelmazouzi@gmail.com>
2025-10-19 23:38:39 +0200

    1ere version du Modèle et test

--- src/CalculatorModel.java
+++ src/CalculatorModel.java
@@ -1,3 +1,98 @@
-public class CalculatorModel {
+import java.util.Stack;
+
+// A corriger : Certaines opérations ne donnent pas l'identique à une vraie calculatrice RPN
+// Ex : 5 push 3 push multiply donne 15 OK
+//      8 push 6 push substract, substract, substract donne autre résultat.
+
+interface CalculatorModelInterface {
+    void add();
+    void subtract();
+    void multiply();
+    void divide();
+    void opposite();
+    void push();
+    void pop();
+    void drop();
+    void swap();
+    void clear();
+}
+
+
+public class CalculatorModel implements CalculatorModelInterface {
+
+    private double accu;    // Accumulateur : Valeur en cours de traitement (affichée par la calculatrice)
+
+    public double getAccu(){
+        return this.accu;
+    } 
+
+    public void setAccu(double value){  
+        this.accu = value;
+    }
+
+    Stack<Double> memory = new Stack<>(); // Mémoire pile
+
+
+    // Definition des opérations de la calculatrice RPN à implémenter
+    @Override
+    public void add() {
+        accu = memory.pop() + accu;         // Additionne accu avec la dernière valeur en mémoire
+        memory.push(accu);                  // Remet le résultat dans la pile
+    }
+
+    @Override
+    public void subtract(){
+        accu = memory.pop() - accu;
+        memory.push(accu);                 // Remet le résultat dans la pile
+    }
+
+    @Override
+    public void multiply(){
+        accu = memory.pop() * accu;
+        memory.push(accu);                // Remet le résultat dans la pile
+    }
+
+    @Override
+    public void divide(){
+        accu += memory.pop() / accu;
+        memory.push(accu);                // Remet le résultat dans la pile
+    }
+
+    @Override
+    public void opposite(){ // Change le signe de la dernière valeur de la pile
+        double A = memory.pop();
+        memory.push(-A);                   // Remet le résultat dans la pile
+    }
+
+    @Override
+    public void push(){
+        memory.push(accu);
+
+    }
     
+    @Override
+    public void pop(){
+        accu = memory.pop();
+        // fixer try catch : si accumulateur vide, erreur
+    }
+    
+    @Override
+    public void drop(){         // supprime la dernière valeur de la pile
+        memory.pop();
+    }
+
+    @Override
+    public void swap(){         // echange les deux dernières valeurs de la pile
+        double last = memory.pop();
+        double second = memory.pop();
+        memory.push(last);      // second position
+        memory.push(second);    // last position
+
+    }
+
+    @Override
+    public void clear(){        // vide la pile
+        memory.clear();
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,88 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        CalculatorModel calc = new CalculatorModel();
+
+        calc.setAccu(2.0);
+        System.out.println("Initial accu: " + calc.getAccu());
+        System.out.println("[Pushing accu to memory]");
+        
+        calc.push();
+        System.out.println("Memory after push: " + calc.memory);
+
+
+        System.out.println("\n-----------------[Test Addition] -------------------");
+        calc.setAccu(3.0);
+        System.out.println("New accu: " + calc.getAccu());
+
+        calc.add(); // On appuie sur le bouton "+"
+        System.out.println("Accu after addition: " + calc.getAccu());
+        System.out.println("Memory after addition: " + calc.memory);
+
+
+        System.out.println("\n-----------------[Test Multiplication] -------------------");
+        calc.setAccu(8);
+        System.out.println("New accu: " + calc.getAccu());
+
+        calc.multiply();
+        System.out.println("Accu after multiplication "+ calc.getAccu());
+        System.out.println("Memory after multiplication: " + calc.memory);
+
+
+        System.out.println("\n----------[Test push plusieurs valeurs en memoire] ------------");
+        calc.setAccu(2);
+        System.out.println("New accu: " + calc.getAccu());
+        calc.push();
+        System.out.println("Memory after push: " + calc.memory +"<-- Last value (displayed on the screen)");
+
+
+        System.out.println("\n-----------------[Test Soustraction] -------------------");
+        calc.setAccu(5);
+        System.out.println("New accu: " + calc.getAccu());
+        calc.subtract();
+        System.out.println("Accu after subtraction: " + calc.getAccu());
+        System.out.println("Memory after subtraction: " + calc.memory);
+
+
+        System.out.println("\n----------[Test 2 soustractions consecutives] ------------");
+        calc.clear();
+        System.out.println("Memory after clear: " + calc.memory);
+
+        // Essaye avec une vraie calculatrice RPN : 
+        calc.setAccu(4);
+        System.out.println("New accu: " + calc.getAccu());
+        calc.push();
+        System.out.println("Memory after push: " + calc.memory);
+        calc.subtract();
+        System.out.println("Accu after subtraction: " + calc.getAccu());
+
+
+        System.out.println("\n----------[Test Soustraction puis multiplication] ------------");
+        calc.clear();
+        System.out.println("Memory after clear: " + calc.memory);
+
+        calc.setAccu(6);
+        System.out.println("New accu: " + calc.getAccu());
+        calc.push();
+        System.out.println("Memory after push: " + calc.memory);
+
+        calc.setAccu(8);
+        System.out.println("New accu: " + calc.getAccu());
+        calc.push();
+        System.out.println("Memory after push: " + calc.memory);
+
+        calc.setAccu(3);
+        System.out.println("New accu: " + calc.getAccu());
+        calc.push();
+        System.out.println("Memory after push: " + calc.memory);
+
+        calc.add();
+        System.out.println("Memory after addition: " + calc.memory);
+
+        calc.multiply();
+        System.out.println("Memory after multiplication: " + calc.memory);
+
+        calc.subtract();
+        System.out.println("Memory after subtraction: " + calc.memory);
+        // La calculatrice ne gère pas correctement la pile
     }
 }
